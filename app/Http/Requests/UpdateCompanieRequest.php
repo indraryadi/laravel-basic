43f003c4e4631f06f267566a10bc5ad31bfd500a
indraryadi <indraaryadikadek@gmail.com>
2022-09-08 09:17:41 -0400

    add Update Request validation

---
+++ app/Http/Requests/UpdateCompanieRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCompanieRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'email'=>['required','email','unique:companies,email,'.$this->companie->id],
+            'logo' => 'nullable|mimes:jpg,jpeg,png|image|max:2048',
+            'website' => 'required',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'name' => 'name is required',
+            'email' => 'email is required',
+            'logo' => 'logo is required',
+            'website' => 'website is required',
+
+        ];
+    }
+}
